754bd8df684f8a4205eb32a9adb9c7d9ba841099
Cam <cam@camm.id.au>
2023-06-08 13:12:46 +1000

    fix #229

--- Checks/check-ORCA229.ps1
+++ Checks/check-ORCA229.ps1
@@ -21,7 +21,7 @@ class ORCA229 : ORCACheck
         $this.Area="Microsoft Defender for Office 365 Policies"
         $this.Name="Anti-phishing trusted domains"
         $this.PassText="No trusted domains in Anti-phishing policy"
-        $this.FailRecommendation="Remove whitelisting on domains in Anti-phishing policy"
+        $this.FailRecommendation="Remove allow listing on domains in Anti-phishing policy"
         $this.Importance="Adding domains as trusted in Anti-phishing policy will result in the action for protected domains, protected users or mailbox intelligence protection will be not applied to messages coming from these sender domains. If a trusted domain needs to be added based on organizational requirements it should be reviewed regularly and updated as needed. We also do not recommend adding domains from shared services."
         $this.ExpandResults=$True
         $this.CheckType=[CheckType]::ObjectPropertyValue
